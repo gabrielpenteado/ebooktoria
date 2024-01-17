16e2cbd46c2081076ec5a9704863935e9c76f443
Gabriel Penteado <gabripenteado@gmail.com>
2024-01-17 00:50:53 -0300

    changed apollo-client uri from local development to production

--- src/providers/apollo-provider/Provider.tsx
+++ src/providers/apollo-provider/Provider.tsx
@@ -9,8 +9,8 @@ import {
 
 export const ProviderApollo = ({ children }: { children: React.ReactNode }) => {
   const client = new ApolloClient({
-    uri: "http://localhost:3000/api/graphql", // for local development
-    // uri: "https://ebooktoria.vercel.app/api/graphql", // for production
+    // uri: "http://localhost:3000/api/graphql", // for local development
+    uri: "https://ebooktoria.vercel.app/api/graphql", // for production
     cache: new InMemoryCache(),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
